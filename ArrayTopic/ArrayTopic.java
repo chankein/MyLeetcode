b18f56a0857ff3ff8f7df8b1884a74786d2cdba9
chankein <chankeinjp@yahoo.co.jp>
2022-07-19 20:03:26 +0900

    finished question 16

--- ArrayTopic/ArrayTopic.java
+++ ArrayTopic/ArrayTopic.java
@@ -204,6 +204,41 @@ n == height.length
         }
         return result;
     }
+
+/*
+16. 最接近的三数之和
+给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+
+返回这三个数的和。
+
+假定每组输入只存在恰好一个解。
+ */
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest_num = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length -1;
+            while (left < right){
+                int this_result = nums[i] + nums[left] + nums[right];
+                if(this_result == target){
+                    return target;
+                } 
+                if(this_result > target){
+                    right--;
+                } else{
+                    left++;
+                }
+                if(Math.abs(this_result-target)<Math.abs(closest_num-target)){
+                    closest_num=this_result;
+                }
+            }
+        }
+        return closest_num;
+    }
     public static void main(String[] args) {
         //int nums[] = new int[]{2, 5, 7, 8};
         //TwoSum(nums, 10);
