99a5307e4fc0b9c5745b6794911e5192e5305b81
chankein <chankeinjp@yahoo.co.jp>
2022-07-23 23:05:05 +0900

    add new question

--- String/Solution.java
+++ String/Solution.java
@@ -137,6 +137,28 @@ class Solution {
         return result;
     }
 }
+
+/*
+383. 赎金信
+给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+
+如果可以，返回 true ；否则返回 false 。
+
+magazine 中的每个字符只能在 ransomNote 中使用一次。
+*/
+public boolean canConstruct(String ransomNote, String magazine) {
+        int[] counter = new int[26];
+        for (int i=0;i< magazine.length(); i++){
+            counter[magazine.charAt(i)-'a'] += 1;
+        }
+        for (int i=0;i< ransomNote.length(); i++){
+            counter[ransomNote.charAt(i)-'a'] -= 1;
+            if(counter[ransomNote.charAt(i)-'a']<0){
+                return false;
+            }
+        } 
+        return true;
+}
 public static void main(String[] args) {
     Solution.romanToInt('XXVII');
 }
