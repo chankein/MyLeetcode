fac20abf1d9be8c685dc800813ed860cada39325
chankein <chankeinjp@yahoo.co.jp>
2022-07-28 22:46:49 +0900

    add No.168

--- Math/Solution.java
+++ Math/Solution.java
@@ -75,4 +75,32 @@ class Solution {
         }
         return true;
     }
+
+/**
+168. Excel表列名称
+给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+*/
+    public String convertToTitle(int columnNumber) {
+    int res = columnNumber;
+    StringBuffer an = new StringBuffer();
+    while (res > 0){
+        int a0 = (res - 1) % 26 + 1;
+        an.append((char)(a0 - 1 + 'A'));
+        res = (res-a0)/26;
+    }
+    an.reverse();
+    return an.toString();
+    }
 }
