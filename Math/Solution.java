679ae596f5e6daf00396e3cac1472c995e35d2c4
chankein <chankeinjp@yahoo.co.jp>
2022-07-29 23:44:33 +0900

    add No.171

--- Math/Solution.java
+++ Math/Solution.java
@@ -103,4 +103,44 @@ AB -> 28
     an.reverse();
     return an.toString();
     }
+
+/**
+171. Excel 表列序号
+给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回 该列名称对应的列序号 。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+示例 1:
+
+输入: columnTitle = "A"
+输出: 1
+示例 2:
+
+输入: columnTitle = "AB"
+输出: 28
+示例 3:
+
+输入: columnTitle = "ZY"
+输出: 701
+ 
+ */
+    public int titleToNumber(String columnTitle) {
+        int this_size = columnTitle.length();
+        int an = 0;
+        for (int i=this_size;i>0;i--){
+            int this_num = columnTitle.charAt(i-1)-'A'+1;
+            an += this_num*Math.pow(26,this_size-i);
+        }
+        return an;
+    }
 }
