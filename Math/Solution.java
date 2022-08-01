ec3974ac4e0362275e8adb7bbf3d07aafc56ea0a
chankein <chankeinjp@yahoo.co.jp>
2022-08-01 23:34:49 +0900

    add No.202

--- Math/Solution.java
+++ Math/Solution.java
@@ -176,4 +176,52 @@ AB -> 28
         return ans;
     }
 
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+/*
+202. 快乐数
+编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」 定义为：
+
+对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+如果这个过程 结果为 1，那么这个数就是快乐数。
+如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+
+ 
+
+示例 1：
+
+输入：n = 19
+输出：true
+解释：
+1*1 + 9*9 = 82
+8*8 + 2*2 = 68
+6*6 + 8*8 = 100
+1*1 + 0*0 + 0*0 = 1
+示例 2：
+
+输入：n = 2
+输出：false
+*/
+    public boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+
 }
